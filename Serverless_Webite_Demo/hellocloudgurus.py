87cbca4316073a096a8e0ece00ba9ecdf060224f
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2020-10-13 16:21:13 +0100

    Add files via upload

---
+++ Serverless_Webite_Demo/hellocloudgurus.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Keep being awesome Cloud Gurus!"
+    }
+    
+    return resp
