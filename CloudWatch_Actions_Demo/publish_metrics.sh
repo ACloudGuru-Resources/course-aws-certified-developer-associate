dccc1d444701d3b3ec5002bce97480121db00790
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2021-12-21 15:07:50 +0000

    Create publish_metrics.sh
    Added file: publish_metrics.sh

---
+++ CloudWatch_Actions_Demo/publish_metrics.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:05:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:06:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:07:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:08:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:09:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:10:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:11:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:12:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:13:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:14:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:15:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:16:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:17:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:18:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:19:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:20:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2021-12-21T11:21:00.000Z
