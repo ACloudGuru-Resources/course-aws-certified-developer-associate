6c9f5ce4bb6dcd2df0031aa305b5d23d4ddac0bd
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2022-10-06 10:19:20 +0100

    Delete CloudFormation_&_SAM_Demo directory

--- CloudFormation_&_SAM_Demo/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = (event, context, callback) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello Cloud Gurus, This Lambda Function was deployed using SAM!')
-    };
-    callback(null, response);
-};
