fad013690fa9cc3d048fc4f953f86d8ed8b781fa
Chris Perry <gameboy9478@gmail.com>
2024-01-10 10:16:00 -0600

    Fix RDS Demo bootstrap script error

--- RDS_Demo/bootstrap.sh
+++ RDS_Demo/bootstrap.sh
@@ -1,3 +1,6 @@
 #!/bin/bash  
-yum update -y
-yum install mysql -y
+sudo dnf update -y
+wget https://dev.mysql.com/get/mysql80-community-release-el9-3.noarch.rpm -y
+sudo dnf install mysql80-community-release-el9-3.noarch.rpm -y
+sudo yum update -y
+sudo yum install mysql-community-client -y
